511ad0bf31ddbf48a51bd79a47b920a2af40cca6
riinajh <jhriina@gmail.com>
2025-05-07 15:35:39 +0200

    adding example data

---
+++ example_data/README_transitions_data.txt
@@ -0,0 +1,13 @@
+Note: The conversion from binary to decimal is done by increasing weights from left to right (as opposed to the usual convention of right to left). 
+Thus, 1 is 100, 3 is 110, 4 is 001, etc. 
+
+Metadata: 3-locus model for competition on 2 resources in a chemostat. 
+The trait values of the 8 genotypes are:
+0 [ 2.2452433   3.44411449]
+1 [ 2.83925786  0.09714052]
+2 [ 0.37287051  5.18945025]
+3 [11.93348629  0.51969349]
+4 [ 8.16413095 10.90148293]
+5 [ 0.58620779  1.07459635]
+6 [ 0.59765222  0.01669213]
+7 [ 5.23308326  0.08151759]
--- main.R
+++ main.R
@@ -1,8 +1,56 @@
 library(tidyverse)
 library(shiny)
-# library(plotly) we actually don't need this since chord diagrams aren't compatible with ggplot
-library(chorddiag)
-g <- ggplot(faithful, aes(x = eruptions, y = waiting)) +
-  stat_density_2d(aes(fill = after_stat(level)), geom = "polygon") + 
-  xlim(1, 6) + ylim(40, 100)
-ggplotly(g)
+#library(plotly) #we actually don't need this since chord diagrams aren't compatible with ggplot
+library(circlize)
+
+# data may take the form of an adjacency matrix or dataframe
+# i am starting with a simple csv of first-order transitions
+x <- read.table("example_data/transitions_data.d", header = T, sep = ",")
+
+# we have two arrow types to indicate directionality
+chordDiagram(x, directional = 1, 
+             direction.type = "arrows", 
+             link.arr.type = "big.arrow", 
+             diffHeight = -mm_h(2), 
+             # color ramp can be set to, for instance, the indegree
+             grid.col = colorRampPalette(c("lightblue", "red"))(8),
+             annotationTrack = c("name", "grid"))
+circos.clear()
+      
+#colors of the arrows can be changed
+chordDiagram(x, directional = 1, 
+             direction.type = "arrows", 
+             grid.col = colorRampPalette(c("lightblue", "red"))(8),
+             annotationTrack = c("name", "grid"))
+circos.clear()
+
+
+
+# ok now trying a dataset with higher order interactions (two resources)
+y <- read.table("example_data/transitions_data_full.d", header = T, sep = ";") %>% 
+  drop_na()
+sector_order <- c("(0)", "(1)", "(2)", "(3)",
+           "(4)", "(5)", "(6)", "(7)",
+           "(0,3)","(0,7)","(1,2)","(1,5)",
+           "(2,3)","(2,7)","(5,7)")
+ratio <- c()
+for (state in unique(c(y$From, y$To))){
+  ratio <- c(ratio, sum(str_count(state, y$From)) / sum(c(str_count(state, y$From), str_count(state,y$To))))
+}
+
+# there's some strange problem with (6);(7), it detects the destination 7 as different than
+# the source 7? I cut it out of the data for the time being
+
+chordDiagram(y, order = sector_order,
+             annotationTrack = c("name", "grid"),
+             directional = 1, 
+             direction.type = "arrows")
+circos.clear()
+
+chordDiagram(y, order = sector_order,
+             annotationTrack = c("name", "grid"),
+             directional = 1, 
+             direction.type = "arrows",
+             link.arr.type = "big.arrow", 
+             diffHeight = -mm_h(2))
+circos.clear()
